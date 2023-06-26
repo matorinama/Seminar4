db04fcb472ad19ee704c9fd4b3b3452766094201
Mari <matorina.mariya@mail.ru>
2023-06-26 13:23:12 +0300

    ex25, ex29

---
+++ ex25/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.WriteLine("Введите А: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите натуральное число В: ");
+int B = int.Parse(Console.ReadLine());
+if (B < 0) Console.WriteLine("В не является натуральным числом");
+else
+{
+    int pow = A;
+    for (int i = 1; i < B; i++)
+    {
+        pow = pow * A;
+    }
+    Console.WriteLine($"{A} в {B} степени -> {pow}");
+}
+
---
+++ ex29/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 50);
+
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
